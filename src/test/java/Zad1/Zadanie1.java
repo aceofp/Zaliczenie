dd92de8c3736583627406123386b1f1c08ea4087
kermit <aceofp@gmail.com>
2022-04-03 21:04:30 +0200

    modified lines on Zadanie1

--- src/test/java/Zad1/Zadanie1.java
+++ src/test/java/Zad1/Zadanie1.java
@@ -5,7 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/Cucumber/Features/Zadanie1.feature", plugin = {"pretty", "html:out"}, glue = "StepDefinitions")
+@CucumberOptions(features = "src/Cucumber/Features/Zadanie1.feature",
+        plugin = {"pretty", "html:out"},
+        glue = "StepDefinitions")
 
 public class Zadanie1 {
 
